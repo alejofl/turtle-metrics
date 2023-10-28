5187e1722ac907069c61ab5cb63bb0eddfc74e57
Alejo Flores Lucey <alejo@misterflores.com>
2023-10-27 22:10:57 -0300

    Make TripMapper implement HazelcastInstanceAware

--- api/src/main/java/ar/edu/itba/pod/query1/TripsMapper.java
+++ api/src/main/java/ar/edu/itba/pod/query1/TripsMapper.java
@@ -1,17 +1,39 @@
 package ar.edu.itba.pod.query1;
 
 import ar.edu.itba.pod.IntegerPair;
+import ar.edu.itba.pod.Util;
 import ar.edu.itba.pod.data.Bike;
+import ar.edu.itba.pod.data.Station;
+import com.hazelcast.core.HazelcastInstance;
+import com.hazelcast.core.HazelcastInstanceAware;
 import com.hazelcast.mapreduce.Context;
 import com.hazelcast.mapreduce.Mapper;
 
-public class TripsMapper implements Mapper<Integer, Bike, IntegerPair, Integer> {
+import java.util.Map;
+
+public class TripsMapper implements Mapper<Integer, Bike, IntegerPair, Integer>, HazelcastInstanceAware {
+    private Map<Integer, Station> stations;
+
+    public TripsMapper() {
+    }
+
     @Override
     public void map(Integer integer, Bike bike, Context<IntegerPair, Integer> context) {
         if (bike.getStartStationPK() == bike.getEndStationPK()) {
             return;
         }
 
+        Station stationA = stations.getOrDefault(bike.getStartStationPK(), null);
+        Station stationB = stations.getOrDefault(bike.getEndStationPK(), null);
+        if (stationA == null || stationB == null) {
+            return;
+        }
+
         context.emit(IntegerPair.of(bike.getStartStationPK(), bike.getEndStationPK()), 1);
+    }
+
+    @Override
+    public void setHazelcastInstance(HazelcastInstance hazelcastInstance) {
+        this.stations = hazelcastInstance.getMap(Util.HAZELCAST_NAMESPACE);
     }
 }
--- client/src/main/java/ar/edu/itba/pod/client/query1/TripsBetweenStations.java
+++ client/src/main/java/ar/edu/itba/pod/client/query1/TripsBetweenStations.java
@@ -34,12 +34,8 @@ public class TripsBetweenStations extends QueryClient {
         Map<Integer, Station> stations = getHz().getMap(Util.HAZELCAST_NAMESPACE);
         Set<TripsBetweenStationsResult> results = new TreeSet<>();
         for (Map.Entry<IntegerPair, Integer> entry : reducedData.entrySet()) {
-            Station stationA = stations.getOrDefault(entry.getKey().getKey(), null);
-            Station stationB = stations.getOrDefault(entry.getKey().getValue(), null);
-
-            if (stationA == null || stationB == null) {
-                continue;
-            }
+            Station stationA = stations.get(entry.getKey().getKey());
+            Station stationB = stations.get(entry.getKey().getValue());
 
             results.add(new TripsBetweenStationsResult(
                     stationA.getName(),
